31e29ef5f2878f2f2b2a69d7439ea3fa19aac841
lvaldes22 <lvaldes2211@gmail.com>
2022-11-13 10:37:21 -0500

    five commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./src/database/conexion')
+const app = express();
+dbConnection();
+app.use( cors() );
+app.use( express.json() )
+app.use('/api/pedidos', require('./src/routes/pedidos'));
+app.use('/api/usuarios', require('./src/routes/usuarios'));
+app.use('/api/auth', require('./src/routes/auth'));
+app.use('/api/articulos', require('./src/routes/articulos'));
+app.use('/api/yappy', require('./src/routes/yappy'));
+app.use('/api/archivos', require('./src/routes/archivos'));
+app.use('/api/suscripcion', require('./src/routes/suscripcion'));
+app.listen(process.env.PORT, ()=>{
+    console.log("Server corriendo en puerto",process.env.PORT)
+})
